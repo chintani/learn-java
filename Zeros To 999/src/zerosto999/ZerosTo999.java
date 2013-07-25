25478bcaa4a8d37385cf96f8bd27c30ee3859d22
chintani <cybercorpindustries@gmail.com>
2013-07-25 00:29:11 -0400

    Commit 2.0

---
+++ Zeros To 999/src/zerosto999/ZerosTo999.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zerosto999;
+
+/**
+ *
+ * @author Tania Chin
+ */
+public class ZerosTo999 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        int n;
+        for (n = 1; n <1000;n++){
+            System.out.format("%03d%n", n);    
+     }
+}
+}
