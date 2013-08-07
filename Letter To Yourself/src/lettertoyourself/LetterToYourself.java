9969cadb40237f044d249e7dbc92971dca251274
chintani <cybercorpindustries@gmail.com>
2013-08-07 17:46:37 -0400

    1.0
    Don't know if there is an easier way to do this, but this is what I came
    up with and it works!

---
+++ Letter To Yourself/src/lettertoyourself/LetterToYourself.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lettertoyourself;
+
+/**
+ *
+ * @author Tania Chin
+ */
+public class LetterToYourself {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("+---------------------------------------------+");
+    System.out.println("|                                       ####  |");
+    System.out.println("|                                       ####  |");
+    System.out.println("|                                       ####  |");
+    System.out.println("|                                             |");
+    System.out.println("|                                             |");
+    System.out.println("|               Tania Chin                    |");
+    System.out.println("|               11251 NW 707th Ave            |");
+    System.out.println("|               Fort Lauderdale, FL 33333     |");
+    System.out.println("|                                             |");
+    System.out.println("+---------------------------------------------+");
+	}    
+ 	
+ }
