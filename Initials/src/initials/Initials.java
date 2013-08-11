55e0af9e9bafacfd2c4c00a132c15f275ad1ef5f
chintani <cybercorpindustries@gmail.com>
2013-08-11 18:27:58 -0400

    Commit 1.0

---
+++ Initials/src/initials/Initials.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package initials;
+
+/**
+ *
+ * @author Tania Chin
+ */
+public class Initials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Initials for the name Tania A Chin ...");
+        System.out.println("TTTTT    A     CCC ");
+        System.out.println("  T     A A   C   C");
+        System.out.println("  T    A   A  C    ");
+        System.out.println("  T    AAAAA  C    ");
+        System.out.println("  T    A   A  C    ");
+        System.out.println("  T    A   A  C   C");
+        System.out.println("  T    A   A   CCC ");
+        
+    }
+}
