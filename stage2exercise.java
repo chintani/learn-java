46c6457bbd2659ed8c4b6ea2cbb72aa4811bc249
Tania Chin <cybercorpindustries@gmail.com>
2014-11-05 23:57:41 -0500

    Stage 2 exercise.

---
+++ stage2exercise.java
@@ -0,0 +1,4 @@
+// I've imported java.io.Console for you.  It is stored in a variable called console for you.
+String name = console.readLine("What is your name?  ");
+String pastTenseVerb = console.readLine("Give me a past tense verb:  ");
+console.printf("%s really %s this coding exercise.", name, pastTenseVerb);
